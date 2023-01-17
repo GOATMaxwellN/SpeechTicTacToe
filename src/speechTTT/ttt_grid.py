ea0273f3c12a1506bce75f5decc1320fe5317d41
Maxwell Nganyadi <maxwelltechmaster@outlook.com>
2023-01-17 00:24:11 -0600

    feat: draw grid lines on tic-tac-toe grid
    Grid lines are finished and the grid is now complete

--- src/speechTTT/ttt_grid.py
+++ src/speechTTT/ttt_grid.py
@@ -14,11 +14,18 @@ class TTTGrid(Canvas):
     def __init__(self, root, **options):
         super().__init__(root, options)
 
-    def draw_grid(self):
+    def draw_grid(self) -> None:
         """Draws an empty ttt grid for the start of the game.
         
         First creates the bounding square for the grid then makes
         the lines."""
+        square_id = self.draw_bounding_square()
+        self.draw_grid_lines(square_id)
+
+    def draw_bounding_square(self) -> int:
+        """Draws the bounding square of the tic-tac-toe grid.
+        
+        AKA the border of the grid."""
         # Get height and width of canvas.
         w, h = self.winfo_width(), self.winfo_height()
 
@@ -30,23 +37,38 @@ class TTTGrid(Canvas):
         x_offset = self._get_required_width_offset(w, (grid_y[1] - grid_y[0]))
         grid_x = 0 + x_offset, w - x_offset
 
-        # Draw the bounding square
-        self.create_rectangle(grid_x[0], grid_y[0], grid_x[1], grid_y[1], outline='black')
+        # Draw the bounding square and return its id
+        return self.create_rectangle(grid_x[0], grid_y[0], grid_x[1], grid_y[1], outline="black")
+
+    def draw_grid_lines(self, square_id: int) -> None:
+        """Draws the grid lines in the bounding square, completing the tic-tac-toe grid."""
+        # The top left coords of the bounding square will be treated like the origin.
+        o = self.coords(square_id)
+        # Get gap between each row and column line: (width) / 3.
+        gap = round((o[2] - o[0]) / 3)
+        
+        # Draw the vertical lines.
+        self.create_line(o[0] + gap, o[1], o[0] + gap, o[3])
+        self.create_line(o[0] + 2*gap, o[1], o[0] + 2*gap, o[3])
+
+        # Draw the horizontal lines
+        self.create_line(o[0], o[1] + gap, o[2], o[1] + gap)
+        self.create_line(o[0], o[1] + 2*gap, o[2], o[1] + 2*gap)
 
     def _get_required_width_offset(self, width: int, height_to_match: int) -> int:
         """Gets required offset (margins) to place on the width of the
         tic-toe-toe grid so the width is the same as the height."""
         return round((width - height_to_match) / 2)
 
-    def add_x(self, cell: int):
+    def add_x(self, cell: int) -> None:
         """Adds an X to the specified cell."""
         pass
 
-    def add_o(self, cell: int):
+    def add_o(self, cell: int) -> None:
         """Adds an O to the specified cell."""
         pass
 
-    def grid(self, **options):
+    def grid(self, **options) -> None:
         """Adds TTTGrid to root element with grid geometry manager and 
         draws the tic-tac-toe grid on the canvas.
         
@@ -57,6 +79,13 @@ class TTTGrid(Canvas):
         # Makes sure draw_grid() has the correct width and height for canvas
         self.update_idletasks()
         self.draw_grid()
+
+    def create_line(self, *args, **options):
+        """Overrides Canvas.create_line() to make black the default line color."""
+        if "fill" in options:
+            return super().create_line(*args, **options)
+
+        return super().create_line(*args, **options, fill="black")
 
 
 if __name__ == "__main__":
