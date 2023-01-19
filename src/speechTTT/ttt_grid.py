7002f05d55e62e6497aa5459f480719bac5ebcac
Maxwell Nganyadi <maxwelltechmaster@outlook.com>
2023-01-18 19:30:07 -0600

    feat: create invisible squares to be used for clicks
    Drew invisible squares on the canvas that will be used to detect which
    cell user clicks. This is supposed to be SPEECH tic-tac-toe, but there
    will be click detection as well if user chooses to click.

--- src/speechTTT/ttt_grid.py
+++ src/speechTTT/ttt_grid.py
@@ -12,15 +12,18 @@ class TTTGrid(Canvas):
     Y_OFFSET = 30
 
     def __init__(self, root, **options):
-        super().__init__(root, options)
+        super().__init__(root, **options)
 
     def draw_grid(self) -> None:
         """Draws an empty ttt grid for the start of the game.
         
         First creates the bounding square for the grid then makes
         the lines."""
-        square_id = self.draw_bounding_square()
-        self.draw_grid_lines(square_id)
+        self.square_id = self.draw_bounding_square()
+        self.draw_grid_lines()
+
+        # These are the squares that will detect clicks, if user chooses to click.
+        self.squares = self.draw_squares()
 
     def draw_bounding_square(self) -> int:
         """Draws the bounding square of the tic-tac-toe grid.
@@ -40,10 +43,10 @@ class TTTGrid(Canvas):
         # Draw the bounding square and return its id
         return self.create_rectangle(grid_x[0], grid_y[0], grid_x[1], grid_y[1], outline="black")
 
-    def draw_grid_lines(self, square_id: int) -> None:
+    def draw_grid_lines(self) -> None:
         """Draws the grid lines in the bounding square, completing the tic-tac-toe grid."""
         # The top left coords of the bounding square will be treated like the origin.
-        o = self.coords(square_id)
+        o = self.coords(self.square_id)
         # Get gap between each row and column line: (width) / 3.
         gap = round((o[2] - o[0]) / 3)
         
@@ -54,6 +57,36 @@ class TTTGrid(Canvas):
         # Draw the horizontal lines
         self.create_line(o[0], o[1] + gap, o[2], o[1] + gap)
         self.create_line(o[0], o[1] + 2*gap, o[2], o[1] + 2*gap)
+
+    def draw_squares(self):
+        """Draws invisible squares that will be used to detect when user clicks on a
+        cell on the tic-tac-toe grid.
+        
+        They're invisible because the border and grid lines already create the
+        effect of a tic-tac-toe grid, so there is no need to give them any
+        appearance."""
+        # The top left coords of the bounding will be treated like the origin.
+        o = self.coords(self.square_id)
+        # Get side lengths of the squares
+        side_length = round((o[2] - o[0]) / 3)
+
+        squares = []
+        row = col = 0
+        # Squares are created from the top left, row by row.
+        for i in range(9): 
+            w_offset, h_offset = col*side_length, row*side_length
+            rect_id = self.create_rectangle(
+                o[0] + w_offset, o[1] + h_offset, 
+                o[0] + w_offset + side_length, o[1] + h_offset + side_length,
+                outline="")  # outline="" gives no outline to the rectangle, would default to black.
+            squares.append(rect_id)
+
+            row += 1
+            if row % 3 == 0:
+                row = 0
+                col += 1
+        
+        return squares
 
     def _get_required_width_offset(self, width: int, height_to_match: int) -> int:
         """Gets required offset (margins) to place on the width of the
@@ -75,12 +108,12 @@ class TTTGrid(Canvas):
         Overrides the original grid() method, but only difference is 
         the added call to TTTGrid.draw_grid().
         """
-        super().grid(options)
+        super().grid(**options)
         # Makes sure draw_grid() has the correct width and height for canvas
         self.update_idletasks()
         self.draw_grid()
 
-    def create_line(self, *args, **options):
+    def create_line(self, *args, **options) -> int:
         """Overrides Canvas.create_line() to make black the default line color."""
         if "fill" in options:
             return super().create_line(*args, **options)
